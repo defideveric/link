4c3ce5015d34176fd1b5a4567e9aebaba3de91a7
erichudson <defideveric@gmail.com>
2024-05-28 18:02:21 -0400

    assets folder added to project

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function TopBar() {
   return (
-    <main>
-      Top Bar
-    </main>
+    <nav className="topbar">
+      <Link href='/' className="flex items-center gap-4">
+        <Image src='/assets/logo.svg' alt="logo" width={28} height={28}/>
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          Link
+        </p>
+      </Link>
+    </nav>
   );
 }
